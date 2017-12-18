2e81f38fbea1642d611dad382da6f2341f3e85a8
Kyle Francis M. Boclot <kboclot@gbox.adnu.edu.ph>
2017-12-18 21:55:38 +0800

    comment for MainMenu.cs git purposes

--- TooLearnOfficial/MainMenu.cs
+++ TooLearnOfficial/MainMenu.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TooLearnOfficial
+namespace TooLearnOfficial //commenting for git
 {
     public partial class MainMenu : Form
     {
