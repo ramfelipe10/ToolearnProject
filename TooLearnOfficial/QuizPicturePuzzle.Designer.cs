4951104ca1136aab1108aa81b9f7b3f37320d326
Kyle Francis M. Boclot <kboclot@gbox.adnu.edu.ph>
2017-12-28 10:32:58 +0800

    Picture Puzzle edited without database interaction

--- TooLearnOfficial/QuizPicturePuzzle.Designer.cs
+++ TooLearnOfficial/QuizPicturePuzzle.Designer.cs
@@ -34,8 +34,36 @@
             this.ButtonBack = new Bunifu.Framework.UI.BunifuImageButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.comboBoxQuizSubject = new System.Windows.Forms.ComboBox();
+            this.textBoxQuizTimeLimit = new System.Windows.Forms.TextBox();
+            this.textBoxQuizTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitPicture = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitPicture1 = new System.Windows.Forms.PictureBox();
+            this.splitPicture2 = new System.Windows.Forms.PictureBox();
+            this.splitPicture3 = new System.Windows.Forms.PictureBox();
+            this.splitPicture4 = new System.Windows.Forms.PictureBox();
+            this.splitPicture5 = new System.Windows.Forms.PictureBox();
+            this.splitPicture6 = new System.Windows.Forms.PictureBox();
+            this.splitPicture7 = new System.Windows.Forms.PictureBox();
+            this.splitPicture8 = new System.Windows.Forms.PictureBox();
+            this.splitPicture9 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.ButtonBack)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture9)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuElipse1
@@ -77,12 +105,187 @@
             this.label1.TabIndex = 126;
             this.label1.Text = "Create Picture Puzzle Quiz";
             // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(300, 175);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(165, 17);
+            this.label14.TabIndex = 133;
+            this.label14.Text = "(Time Limit Per Question)";
+            // 
+            // comboBoxQuizSubject
+            // 
+            this.comboBoxQuizSubject.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxQuizSubject.FormattingEnabled = true;
+            this.comboBoxQuizSubject.Location = new System.Drawing.Point(138, 129);
+            this.comboBoxQuizSubject.Name = "comboBoxQuizSubject";
+            this.comboBoxQuizSubject.Size = new System.Drawing.Size(246, 32);
+            this.comboBoxQuizSubject.TabIndex = 132;
+            // 
+            // textBoxQuizTimeLimit
+            // 
+            this.textBoxQuizTimeLimit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQuizTimeLimit.Location = new System.Drawing.Point(138, 167);
+            this.textBoxQuizTimeLimit.Name = "textBoxQuizTimeLimit";
+            this.textBoxQuizTimeLimit.Size = new System.Drawing.Size(158, 31);
+            this.textBoxQuizTimeLimit.TabIndex = 131;
+            // 
+            // textBoxQuizTitle
+            // 
+            this.textBoxQuizTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQuizTitle.Location = new System.Drawing.Point(138, 89);
+            this.textBoxQuizTitle.Name = "textBoxQuizTitle";
+            this.textBoxQuizTitle.Size = new System.Drawing.Size(400, 31);
+            this.textBoxQuizTitle.TabIndex = 130;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 24);
+            this.label5.TabIndex = 129;
+            this.label5.Text = "Time Limit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "Subject:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 127;
+            this.label2.Text = "Title:";
+            // 
+            // splitPicture
+            // 
+            this.splitPicture.Location = new System.Drawing.Point(50, 214);
+            this.splitPicture.Name = "splitPicture";
+            this.splitPicture.Size = new System.Drawing.Size(113, 109);
+            this.splitPicture.TabIndex = 134;
+            this.splitPicture.TabStop = false;
+            this.splitPicture.Click += new System.EventHandler(this.splitPicture_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 135;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitPicture1
+            // 
+            this.splitPicture1.Location = new System.Drawing.Point(265, 214);
+            this.splitPicture1.Name = "splitPicture1";
+            this.splitPicture1.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture1.TabIndex = 136;
+            this.splitPicture1.TabStop = false;
+            this.splitPicture1.Click += new System.EventHandler(this.splitPicture1_Click);
+            // 
+            // splitPicture2
+            // 
+            this.splitPicture2.Location = new System.Drawing.Point(265, 273);
+            this.splitPicture2.Name = "splitPicture2";
+            this.splitPicture2.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture2.TabIndex = 137;
+            this.splitPicture2.TabStop = false;
+            // 
+            // splitPicture3
+            // 
+            this.splitPicture3.Location = new System.Drawing.Point(265, 329);
+            this.splitPicture3.Name = "splitPicture3";
+            this.splitPicture3.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture3.TabIndex = 138;
+            this.splitPicture3.TabStop = false;
+            // 
+            // splitPicture4
+            // 
+            this.splitPicture4.Location = new System.Drawing.Point(342, 214);
+            this.splitPicture4.Name = "splitPicture4";
+            this.splitPicture4.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture4.TabIndex = 139;
+            this.splitPicture4.TabStop = false;
+            // 
+            // splitPicture5
+            // 
+            this.splitPicture5.Location = new System.Drawing.Point(342, 273);
+            this.splitPicture5.Name = "splitPicture5";
+            this.splitPicture5.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture5.TabIndex = 140;
+            this.splitPicture5.TabStop = false;
+            // 
+            // splitPicture6
+            // 
+            this.splitPicture6.Location = new System.Drawing.Point(342, 329);
+            this.splitPicture6.Name = "splitPicture6";
+            this.splitPicture6.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture6.TabIndex = 141;
+            this.splitPicture6.TabStop = false;
+            // 
+            // splitPicture7
+            // 
+            this.splitPicture7.Location = new System.Drawing.Point(420, 214);
+            this.splitPicture7.Name = "splitPicture7";
+            this.splitPicture7.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture7.TabIndex = 142;
+            this.splitPicture7.TabStop = false;
+            // 
+            // splitPicture8
+            // 
+            this.splitPicture8.Location = new System.Drawing.Point(420, 273);
+            this.splitPicture8.Name = "splitPicture8";
+            this.splitPicture8.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture8.TabIndex = 143;
+            this.splitPicture8.TabStop = false;
+            // 
+            // splitPicture9
+            // 
+            this.splitPicture9.Location = new System.Drawing.Point(420, 329);
+            this.splitPicture9.Name = "splitPicture9";
+            this.splitPicture9.Size = new System.Drawing.Size(62, 50);
+            this.splitPicture9.TabIndex = 144;
+            this.splitPicture9.TabStop = false;
+            // 
             // QuizPicturePuzzle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(1045, 615);
+            this.Controls.Add(this.splitPicture9);
+            this.Controls.Add(this.splitPicture8);
+            this.Controls.Add(this.splitPicture7);
+            this.Controls.Add(this.splitPicture6);
+            this.Controls.Add(this.splitPicture5);
+            this.Controls.Add(this.splitPicture4);
+            this.Controls.Add(this.splitPicture3);
+            this.Controls.Add(this.splitPicture2);
+            this.Controls.Add(this.splitPicture1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitPicture);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.comboBoxQuizSubject);
+            this.Controls.Add(this.textBoxQuizTimeLimit);
+            this.Controls.Add(this.textBoxQuizTitle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ButtonBack);
@@ -91,6 +294,16 @@
             this.Text = "QuizPicturePuzzle";
             ((System.ComponentModel.ISupportInitialize)(this.ButtonBack)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPicture9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,5 +315,23 @@
         private Bunifu.Framework.UI.BunifuImageButton ButtonBack;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.ComboBox comboBoxQuizSubject;
+        private System.Windows.Forms.TextBox textBoxQuizTimeLimit;
+        private System.Windows.Forms.TextBox textBoxQuizTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox splitPicture;
+        private System.Windows.Forms.PictureBox splitPicture9;
+        private System.Windows.Forms.PictureBox splitPicture8;
+        private System.Windows.Forms.PictureBox splitPicture7;
+        private System.Windows.Forms.PictureBox splitPicture6;
+        private System.Windows.Forms.PictureBox splitPicture5;
+        private System.Windows.Forms.PictureBox splitPicture4;
+        private System.Windows.Forms.PictureBox splitPicture3;
+        private System.Windows.Forms.PictureBox splitPicture2;
+        private System.Windows.Forms.PictureBox splitPicture1;
     }
 }
--- TooLearnOfficial/QuizPicturePuzzle.cs
+++ TooLearnOfficial/QuizPicturePuzzle.cs
@@ -23,5 +23,60 @@ namespace TooLearnOfficial
             MainMenu mm = new MainMenu();
             mm.Show();
         }
+
+        private void splitPicture_Click(object sender, EventArgs e) //iclick ang picturebox para magbrowse kang mga image tapos ma automatic ma split
+        {
+            var imgarray = new Image[9];
+            var img = Image.FromFile(@"C:\Users\BOC\Documents\GitHub\ToolearnProject\TooLearnOfficial\TooLearn Pictures\After-Effects-and-Cinema-4D-Flat-Design-Animation-Tutorial.jpg", true); //change code to query image into database
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var index = i * 3 + j;
+                    imgarray[index] = new Bitmap(104, 104);
+                    var graphics = Graphics.FromImage(imgarray[index]);
+                    graphics.DrawImage(img, new Rectangle(0, 0, 104, 104), new Rectangle(i * 150, j * 150, 150, 150), GraphicsUnit.Pixel);
+                    graphics.Dispose();
+                }
+            }
+            splitPicture1.Image = imgarray[0];
+            splitPicture2.Image = imgarray[1];
+            splitPicture3.Image = imgarray[2];
+            splitPicture4.Image = imgarray[3];
+            splitPicture5.Image = imgarray[4];
+            splitPicture6.Image = imgarray[5];
+            splitPicture7.Image = imgarray[6];
+            splitPicture8.Image = imgarray[7];
+            splitPicture9.Image = imgarray[8];
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //ma upload ning image para maglaog sa database.
+        {
+            String imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files|*.png| All Files(*.*)|*.*";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    splitPicture.ImageLocation = imageLocation;
+                }
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void splitPicture1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
