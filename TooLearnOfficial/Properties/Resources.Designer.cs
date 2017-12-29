10285448a0b053e883f8a51ab516ee2521f082be
ramfelipe10 <rfelipe@gbox.adnu.edu.ph>
2017-12-30 01:12:41 +0800

    bg

--- TooLearnOfficial/Properties/Resources.Designer.cs
+++ TooLearnOfficial/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace TooLearnOfficial.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg {
+            get {
+                object obj = ResourceManager.GetObject("bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Checked_40px {
             get {
                 object obj = ResourceManager.GetObject("Checked_40px", resourceCulture);
