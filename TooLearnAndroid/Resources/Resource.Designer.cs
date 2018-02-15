fae0181f7f15b26e8407769ea27d2f0c4f0c2622
Kyle Francis M. Boclot <kboclot@gbox.adnu.edu.ph>
2018-02-15 22:00:54 +0800

    semi push. not responding vs :(

--- TooLearnAndroid/Resources/Resource.Designer.cs
+++ TooLearnAndroid/Resources/Resource.Designer.cs
@@ -45,10 +45,25 @@ namespace TooLearnAndroid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int classroom_button = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int logo = 2130837505;
+			public const int divider = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int edittext_design = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int logo = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int public_button = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int rounded_edittext = 2130837510;
 			
 			static Drawable()
 			{
@@ -63,8 +78,41 @@ namespace TooLearnAndroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f060005
+			public const int button1 = 2131099653;
+			
+			// aapt resource value: 0x7f06000b
+			public const int button2 = 2131099659;
+			
+			// aapt resource value: 0x7f060001
+			public const int editText1 = 2131099649;
+			
+			// aapt resource value: 0x7f060003
+			public const int editText2 = 2131099651;
+			
+			// aapt resource value: 0x7f060008
+			public const int editText3 = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int editText4 = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int editText5 = 2131099658;
+			
 			// aapt resource value: 0x7f060000
 			public const int imageView1 = 2131099648;
+			
+			// aapt resource value: 0x7f060007
+			public const int relativeLayout1 = 2131099655;
+			
+			// aapt resource value: 0x7f060002
+			public const int textView1 = 2131099650;
+			
+			// aapt resource value: 0x7f060004
+			public const int textView2 = 2131099652;
+			
+			// aapt resource value: 0x7f060006
+			public const int textView3 = 2131099654;
 			
 			static Id()
 			{
@@ -80,7 +128,13 @@ namespace TooLearnAndroid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int activity_signin = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int activity_signup = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
