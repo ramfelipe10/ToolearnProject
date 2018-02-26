332f36e56e5c96fd93e1d0d72164028d8180907f
Kyle Francis M. Boclot <kboclot@gbox.adnu.edu.ph>
2018-02-26 22:39:58 +0800

    pushtry

--- TooLearnAndroid/obj/Debug/android/src/mono/MonoPackageManager.java
+++ TooLearnAndroid/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -107,19 +107,6 @@ class MonoPackageManager_Resources {
 	public static final String[] Assemblies = new String[]{
 		/* We need to ensure that "TooLearnAndroid.dll" comes first in this list. */
 		"TooLearnAndroid.dll",
-		"EDMTDev.dll",
-		"Xamarin.Android.Arch.Core.Common.dll",
-		"Xamarin.Android.Arch.Lifecycle.Common.dll",
-		"Xamarin.Android.Arch.Lifecycle.Runtime.dll",
-		"Xamarin.Android.Support.Animated.Vector.Drawable.dll",
-		"Xamarin.Android.Support.Annotations.dll",
-		"Xamarin.Android.Support.Compat.dll",
-		"Xamarin.Android.Support.Core.UI.dll",
-		"Xamarin.Android.Support.Core.Utils.dll",
-		"Xamarin.Android.Support.Fragment.dll",
-		"Xamarin.Android.Support.Media.Compat.dll",
-		"Xamarin.Android.Support.v7.AppCompat.dll",
-		"Xamarin.Android.Support.Vector.Drawable.dll",
 	};
 	public static final String[] Dependencies = new String[]{
 	};
